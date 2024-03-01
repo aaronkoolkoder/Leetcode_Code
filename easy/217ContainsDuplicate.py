5174ccdd26e6bf01b287591d8fb3a430ace6d5bf
aaronkoolkoder <151795610+aaronkoolkoder@users.noreply.github.com>
2024-03-01 15:38:28 -0500

    Update 217ContainsDuplicate.py

--- easy/217ContainsDuplicate.py
+++ easy/217ContainsDuplicate.py
@@ -1,1 +1,12 @@
-#217 Contains Duplicate
+# 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+# Aaron Merchant
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
